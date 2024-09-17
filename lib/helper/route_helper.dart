cfcfe8bede2de6a9ac6ffa249ddd5d3dfe802696
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-09-17 20:30:06 +0000

    fix: Resolve erro de import

--- lib/helper/route_helper.dart
+++ lib/helper/route_helper.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:beeai/features/onboarding/onboarding_screen.dart';
+import 'package:beeai/features/onboarding/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
