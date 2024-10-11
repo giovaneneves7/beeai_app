5dee300ed66eee82eaca2bc8d0038f33c1f49766
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-10-11 18:02:30 +0000

    fix: Resolve erro de controller

--- lib/features/onboarding/screens/onboarding_screen1.dart
+++ lib/features/onboarding/screens/onboarding_screen1.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 
 class OnboardingPage1 extends StatelessWidget {
+  final PageController controller; 
+
+  OnboardingPage1({required this.controller}); 
+
   @override
   Widget build(BuildContext context) {
-    // Acessando o tema com Theme.of(context)
     final theme = Theme.of(context);
 
     return Container(
-      color: theme.primaryColor, // Usando a cor primária do tema
+      color: theme.primaryColor,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Icon(
             Icons.hive_outlined,
             size: 100,
-            color: theme.scaffoldBackgroundColor, // Cor do background
+            color: theme.scaffoldBackgroundColor,
           ),
           SizedBox(height: 20),
           Text(
             "Bem-Vindo Ao Sistema De Monitoramento De Colmeias",
-            style: theme.textTheme.bodyText1!.copyWith(fontSize: 24), // Usando o textTheme do tema
+            style: theme.textTheme.bodyText1!.copyWith(fontSize: 24),
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 20),
           ElevatedButton(
             onPressed: () {
-              PageController controller = PageView.of(context)!;
               controller.nextPage(
                 duration: Duration(milliseconds: 300),
                 curve: Curves.easeInOut,
