01b69f14715b4f4114b2ad53610312e75d4be700
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-10-11 18:52:52 +0000

    feat(homme): Cria home

--- lib/helper/route_helper.dart
+++ lib/helper/route_helper.dart
@@ -4,17 +4,25 @@ import 'package:beeai/features/onboarding/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+/**
+*
+* @author Giovane Neves
+*
+ */
 class RouteHelper{
 
+  static const String home         = '/home';
   static const String notification = '/notification';
-  static const String onboarding = '/onboarding';
+  static const String onboarding   = '/onboarding';
 
+  static String getHomeScreen()         => home;
   static String getNotificationScreen() => notification;
-  static String getOnboardingScreen() => onboarding;
+  static String getOnboardingScreen()   => onboarding;
 
   // Registro de rotas [ Adicionar todas as rotas do app aqui ] 
   static List<GetPage> routes = [
 
+    GetPage(name: home, page: () => HomeScreen()),
   	//GetPage(name: notification, page: () => NotificationScreen()),
     GetPage(name: onboarding, page: () => OnboardingScreen()),
 
