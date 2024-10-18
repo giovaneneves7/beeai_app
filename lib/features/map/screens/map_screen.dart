55e52c9e8b5fac1a0d729e012dbc65ff262ba14c
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-10-18 21:37:31 +0000

    Atualiza mapa

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -2,33 +2,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
-
 class MapScreen extends StatefulWidget {
   @override
   _MapScreenState createState() => _MapScreenState();
 }
 
 class _MapScreenState extends State<MapScreen> {
-
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            body: Stack(
-                children: [
-                FlutterMap(
-                    options: MapOptions(
-                    center: LatLng(-11299, -418568),
-                    zoom: 3.2,
-                    ),
-                    children: [
-                    TileLayer(
-                        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                        userAgentPackageName: 'com.example.app',
-                    ),
-                    ],
-                ),
-                ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FlutterMap(
+            options: MapOptions(
+              // Altere 'center' para 'startCenter'
+              startCenter: LatLng(-11.299, -41.8568), // Corrija os valores de latitude e longitude
+              zoom: 3.2,
             ),
-        );
-    }
+            children: [
+              TileLayer(
+                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                userAgentPackageName: 'com.example.app',
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 }
