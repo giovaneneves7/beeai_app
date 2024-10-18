c00bef12f199b16c70b7145349e6b418fab3740d
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-10-18 21:31:18 +0000

    Atualiza mapa

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -12,27 +12,23 @@ class _MapScreenState extends State<MapScreen> {
 
     @override
     Widget build(BuildContext context) {
-    return FlutterMap(
-        options: MapOptions(
-        initialCenter: LatLng(-11299, -418568), // Center the map over London
-        initialZoom: 9.2,
-        ),
-        children: [
-        TileLayer( // Display map tiles from any source
-            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
-            userAgentPackageName: 'com.example.app',
-            // And many more recommended properties!
-        ),
-        RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
-            attributions: [
-            TextSourceAttribution(
-                'OpenStreetMap contributors',
-                onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
+        return Scaffold(
+            body: Stack(
+                children: [
+                FlutterMap(
+                    options: MapOptions(
+                    center: LatLng(-11299, -418568),
+                    zoom: 3.2,
+                    ),
+                    children: [
+                    TileLayer(
+                        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                    ),
+                    ],
+                ),
+                ],
             ),
-            // Also add images...
-            ],
-        ),
-        ],
-    );
+        );
     }
 }
