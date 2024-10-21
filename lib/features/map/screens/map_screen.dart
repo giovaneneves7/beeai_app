f28c6574edb0c36f617e4b78111ae160699b3ca6
Giovane Neves <123518676+giovaneneves7@users.noreply.github.com>
2024-10-21 22:06:26 +0000

    Adiciona dependência do flutter map location marker

--- lib/features/map/screens/map_screen.dart
+++ lib/features/map/screens/map_screen.dart
@@ -8,39 +8,31 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          FlutterMap(
-            options: MapOptions(
-              center: LatLng(-11.327153, -41.864476), // Centraliza o mapa no local desejado
-              zoom: 13.0, // Define o nível de zoom
-            ),
-            children: [
-              TileLayer(
-                urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                userAgentPackageName: 'com.example.app',
-              ),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    point: LatLng(-11.327153, -41.864476),
-                    width: 80.0,
-                    height: 80.0,
-                    builder: (context) => Icon(
-                      Icons.location_pin,
-                      color: Colors.red,
-                      size: 40.0,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(51.509364, -0.128928), // Center the map over London
+        initialZoom: 9.2,
       ),
+      children: [
+        TileLayer( // Display map tiles from any source
+          urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // OSMF's Tile Server
+          userAgentPackageName: 'com.example.app',
+          // And many more recommended properties!
+        ),
+        CurrentLocationLayer(),
+        RichAttributionWidget( // Include a stylish prebuilt attribution widget that meets all requirments
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // (external)
+            ),
+            // Also add images...
+          ],
+        ),
+      ],
     );
   }
 }
